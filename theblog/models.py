3712de48addd6dc89c9b2d07d1ea9ea3786e7783
Rokas <rookas.rudzenskas@gmail.com>
2021-04-02 12:49:28 +0300

    Updates

--- theblog/models.py
+++ theblog/models.py
@@ -10,4 +10,4 @@ class Post(models.Model):
     body = models.TextField()
 
     def __str__(self):
-        return self.title + ' | ' + self.author
+        return self.title + ' | ' + str(self.author)
