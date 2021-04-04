90d4e8e1e74a109323e5dd76919c9512095612b6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-04 17:57:10 +0300

    Rokas

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
 # from . import views
 urlpatterns = [
     # path('', views.home, name="home"),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
     path('add_category/', AddCategoryView.as_view(), name="add_category"),
+    path('category/<str:cats>/', CategoryView, name="category"),
 
 
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -12,6 +12,13 @@ class HomeView(ListView):
     ordering = ['-post_date']
 
 
+def CategoryView(request, cats):
+
+    category_posts = Post.objects.filter(category=cats)
+
+    return render(request, 'categories.html', {'cats':cats.title, 'category_posts':category_posts})
+
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
