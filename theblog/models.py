c0e784ba03acd4d4ba19a784d5e9540ff8786339
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 16:20:37 +0300

    Rokas

--- theblog/models.py
+++ theblog/models.py
@@ -23,6 +23,7 @@ class Post(models.Model):
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
+    likes = models.ManyToManyField(User, related_name='blog_posts')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
