24cfdf0676d86c8cd7361e0b6271920eef47291e
Rokas <rookas.rudzenskas@gmail.com>
2021-04-08 13:45:11 +0300

    Rokas

--- ablog/settings.py
+++ ablog/settings.py
@@ -115,11 +115,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS(
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 LOGIN_REDIRECT_URL = 'home'
