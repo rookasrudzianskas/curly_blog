e2322210c254134cdc7f78d730bceb371c7b8955
Rokas <rookas.rudzenskas@gmail.com>
2021-04-12 11:18:20 +0300

    Rokas

--- theblog/models.py
+++ theblog/models.py
@@ -30,6 +30,10 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+    def get_absolute_url(self):
+        # return reverse('article-detail', args=(str(self.id)))
+        return reverse('home')
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
