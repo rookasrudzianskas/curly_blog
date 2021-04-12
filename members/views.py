79246861311c6d9da1aa61d6bfad13b9565e878f
Rokas <rookas.rudzenskas@gmail.com>
2021-04-12 11:16:14 +0300

    Rokas

--- members/views.py
+++ members/views.py
@@ -4,17 +4,30 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 
-from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm, ProfilePageForm, EditProfilePageForm
 from theblog.models import Profile
 
 
 # Create your views here.
+
+class CreateProfilePageView(CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    # fields = '__all__'
+    template_name = "registration/create_user_profile_page.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class EditProfilePageView(generic.UpdateView):
     model = Profile
     template_name = 'registration/edit_profile_page.html'
-    fields = ['bio', 'profile_pic', 'website_url', 'twitter_url', 'instagram_url', 'instagram_url', 'pinterest_url']
+    form_class = EditProfilePageForm
+    # fields = ['bio', 'profile_pic', 'website_url', 'twitter_url', 'instagram_url', 'instagram_url', 'pinterest_url']
     success_url = reverse_lazy('home')
 
 
