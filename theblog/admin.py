bfd3276a6fa2a562e71ed0e482d33df309c17fc0
Rokas <rookas.rudzenskas@gmail.com>
2021-04-12 11:49:30 +0300

    Rokas

--- theblog/admin.py
+++ theblog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post, Category, Profile
+from .models import Post, Category, Profile, Comment
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Profile)
+admin.site.register(Comment)
 
